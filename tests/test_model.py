492a33a9ba5a3a7515e42acb18d920d54f3cc3a2
Pavle Jonoski <jonoski.pavle@gmail.com>
2017-04-07 18:09:12 +0200

    Serialize/Parse event tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,10 +1,31 @@
-from theia.model import Event, EventSerializer
+from theia.model import Event, EventSerializer, EventParser
+
+from io import StringIO, BytesIO
 
 from unittest import TestCase
 
 
 class TestEventSerializer(TestCase):
-  
+
   def test_serialize_event(self):
-    ev_str = EventSerializer().serialize(Event(id='id1',source='env1',content='TEST EVENT'))
-    print(ev_str)
+    ev_str = EventSerializer().serialize(Event(id='id1',source='env1', tags=['a', 'b'], content='TEST EVENT'))
+    expected = ['event: 68 58 10', 'id:id1','timestamp:','source:env1','tags:a,b','TEST EVENT']
+    i = 0
+    for line in ev_str.split('\n'):
+      if i == 2:
+        assert line.startswith('timestamp:')
+      else:
+        assert line == expected[i]
+      i += 1
+  
+  def test_parse_event(self):
+    event_str = '\n'.join(['event: 68 58 10', 'id:id1','timestamp: 1491580705.9789374','source:env1','tags:a,b','TEST EVENT'])
+    parser = EventParser()
+    event = parser.parse_event(BytesIO(event_str.encode('utf-8')))
+    
+    assert event
+    assert event.id == 'id1'
+    assert event.source == 'env1'
+    assert event.timestamp == 1491580705.9789374
+    assert event.tags == ['a','b']
+    assert event.content == 'TEST EVENT'
--- theia/model.py
+++ theia/model.py
@@ -2,24 +2,33 @@ from time import time
 from collections import namedtuple
 from io import StringIO
 
-Header = namedtuple('Header', ['id','source','timestamp'])
+
 EventPreamble = namedtuple('EventPreamble', ['total','header','content'])
+
+class Header:
+  
+  def __init__(self, id=None, timestamp=None, source=None, tags=None):
+    self.id = id
+    self.timestamp = timestamp
+    self.source = source
+    self.tags = tags
 
 
 class Event:
-  def __init__(self, id, source, timestamp=None, content=None):
+  def __init__(self, id, source, timestamp=None, tags=None, content=None):
     self.id = id
     self.source = source
     self.timestamp = timestamp or time() # time in nanoseconds UTC
+    self.tags = tags or []
     self.content = content or ''
 
 
 
 class EventSerializer:
-  
+
   def __init__(self, encoding='utf-8'):
     self.encoding = encoding
-  
+
   def serialize(self, event):
     event_str = ''
     hdr = self._serialize_header(event)
@@ -30,72 +39,69 @@ class EventSerializer:
     event_str += hdr
     event_str += event.content
     return event_str
-    
+
   def _serialize_header(self, event):
     hdr = ''
     hdr += 'id:' + str(event.id) + '\n'
-    hdr += 'timestamp:' + str(event.timestamp) + '\n'
+    hdr += 'timestamp: %.7f' % event.timestamp + '\n'
     hdr += 'source:' + str(event.source) + '\n'
+    hdr += 'tags:' + ','.join(event.tags) + '\n'
     return hdr
 
 
 class EventParser:
-  
+
   def __init__(self, encoding='utf-8'):
     self.encoding = encoding
-  
+
   def parse_header(self, hdr_size, stream):
     bytes = stream.read(hdr_size)
     if len(bytes) != hdr_size:
-      raise Exception('Invalid read size from buffer. The stream is either unreadable or corrupted.')
+      raise Exception('Invalid read size from buffer. The stream is either unreadable or corrupted. %d read, expected %d' %(len(bytes), hdr_size))
     hdr_str = bytes.decode(self.encoding)
-    header = EventHeader()
+    header = Header()
     sio = StringIO(hdr_str)
-    
-    ln = sio.readline():
+
+    ln = sio.readline()
     while ln:
       ln = ln.strip()
       if not ln:
         raise Exception('Invalid header')
       idx = ln.index(':')
       prop = ln[0:idx]
-      value = prop[idx+1:]
+      value = ln[idx+1:]
       if prop == 'id':
         header.id = value
       elif prop == 'timestamp':
-        header.timestamp = int(timestamp)
+        header.timestamp = float(value)
       elif prop == 'source':
         header.source = value
+      elif prop == 'tags':
+        header.tags = value.split(',')
       else:
         raise Exception('Unknown property in header %s' % prop)
+      ln = sio.readline()
     sio.close()
     return header
-  
+
   def parse_preamble(self, stream):
     pstr = stream.readline()
     if pstr:
       pstr = pstr.decode(self.encoding).strip()
-    if not pstr or not psrt.startswith('event:'):
+    if not pstr or not pstr.startswith('event:'):
       raise Exception('Invalid preamble line')
-    
+
     values = pstr[len('event:') + 1:].split(' ')
     if len(values) != 3:
       raise Exception('Invalid preamble values')
-    
+
     return EventPreamble(total=int(values[0]), header=int(values[1]), content=int(values[2]))
-  
+
   def parse_event(self, stream):
     preamble = self.parse_preamble(stream)
-    header = self.parse_content(preamble.header, stream)
+    header = self.parse_header(preamble.header, stream)
     content = stream.read(preamble.content)
     if len(content) != preamble.content:
       raise Exception('Invalid content size. The stream is either unreadable or corrupted.')
-    
-    return Event(id=header.id, source=header.source, timestamp=event.timestamp, content=content)
-    
 
-
-
-
-
-
+    return Event(id=header.id, source=header.source, timestamp=header.timestamp, tags=header.tags, content=content.decode(self.encoding))
