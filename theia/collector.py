6d9a218c444be377f242958566d8dd69c6d06456
Pavle Jonoski <jonoski.pavle@gmail.com>
2017-06-26 23:23:13 +0200

    Collector

---
+++ theia/collector.py
@@ -0,0 +1,6 @@
+class Collector:
+
+  def __init__(self, confg):
+    self.confg = confg
+    self.server = None
+  
--- theia/watcher.py
+++ theia/watcher.py
@@ -13,22 +13,22 @@ class FileSource:
     self.enc = enc
     self.tags = tags or []
     self._setup()
-    
+
   def modified(self):
     diff = self._get_diff()
     diff = diff.decode(self.enc)
     self.callback(diff, self.path, self.tags)
-  
+
   def moved(self, dest_path):
     self.path = dest_path
     self.position = 0
-  
+
   def created(self):
     self.position = 0
-    
+
   def removed(self):
     pass
-  
+
   def _get_diff(self):
     with open(self.path, 'rb') as f:
       f.seek(self.position)
@@ -46,31 +46,31 @@ class FileSource:
 
 
 class DirectoryEventHandler(FileSystemEventHandler):
-  
+
   def __init__(self, handlers):
     self.handlers = handlers
-  
+
   def _notify(self, event, *args):
     hnd = self.handlers.get(event)
     if hnd:
       hnd(*args)
-  
+
   def on_moved(self, event):
     self._notify('moved', event.src_path, event.dest_path)
-    
+
   def on_created(self, event):
     self._notify('created', event.src_path)
-    
+
   def on_deleted(self, event):
     self._notify('deleted', event.src_path)
-    
+
   def on_modified(self, event):
     self._notify('modified', event.src_path)
 
 
 
 class SourcesDaemon:
-  
+
   def __init__(self, observer, client, tags=None):
     self.sources = {}
     self.observer = observer
@@ -83,8 +83,8 @@ class SourcesDaemon:
       'modified': self._modified
     })
     self.observer.start()
-    
-  
+
+
   def add_source(self, fpath, enc='UTF-8', tags=None):
     def callback(diff, srcpath, evtags):
       ts = time()
@@ -102,7 +102,7 @@ class SourcesDaemon:
     if files.get(fname):
       return
     files[fname] = fsrc
-  
+
   def remove_source(self, fpath):
     pdir, fname= self._split_path(fpath)
     files = self.sources.get(pdir)
@@ -112,13 +112,13 @@ class SourcesDaemon:
   def _split_path(self, fpath):
     fpath = realpath(fpath)
     return splitpath(fpath)
-  
+
   def _get_file_source(self, src_path):
     pdir, fname = self._split_path(src_path)
     files = self.sources.get(pdir)
     if files:
       return files.get(fname)
-  
+
   def _moved(self, src_path, dest_src):
     pdir, fname = self._split_path(src_path)
     files = self.sources.get(pdir)
@@ -130,17 +130,17 @@ class SourcesDaemon:
         self.sources[ndir] = {}
       self.sources[ndir][nfname] = fsrc
       fsrc.moved(dest_path)
-  
+
   def _created(self, src_path):
     fsrc = self._get_file_source(src_path)
     if fsrc:
       fsrc.created()
-  
+
   def _deleted(self, src_path):
     fsrc = self._get_file_source(src_path)
     if fsrc:
       fsrc.removed()
-  
+
   def _modified(self, src_path):
     fsrc = self._get_file_source(src_path)
     if fsrc:
@@ -149,17 +149,15 @@ class SourcesDaemon:
 
 if __name__ == '__main__':
   from threading import Thread
-  
+
   def run_daemon():
     daemon = SourcesDaemon(observer=Observer(), client=None, tags=['local','test-run'])
-    
-    daemon.add_source(fpath='/tmp/pajo', tags=['retard','file'])
-    
+
+    daemon.add_source(fpath='/tmp/pajo', tags=['file'])
+
     while True:
       sleep(1)
-  
+
   t = Thread(target=run_daemon)
   t.start()
   t.join()
-  
-
