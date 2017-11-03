9463c72fb15f4a1c877042fb082ef1636db9e6a5
Pavle Jonoski <jonoski.pavle@gmail.com>
2017-11-03 01:50:54 +0100

    Output formatting.

--- theia/cli/query.py
+++ theia/cli/query.py
@@ -1,8 +1,10 @@
 from theia.cli.parser import get_parent_parser as parent_parser
 from theia.query import Query
 import asyncio
+import signal
 from theia.model import EventParser
 from io import BytesIO
+from datetime import datetime
 
 def get_parser(subparsers):
   parser = subparsers.add_parser('query', help='Query for events')
@@ -15,6 +17,28 @@ def get_parser(subparsers):
   parser.add_argument('-b', '--before', dest='f_before', metavar='TIMESTAMP', default=0, type=int, help='Match events before this timestamp')
   parser.add_argument('-c', '--content', dest='f_content', metavar='PATTERN', default=None, help='Match event content')
   parser.add_argument('-t', '--tags', nargs='*', dest='f_tags', metavar='PATTERN', help='Match any of the tags')
+  
+  parser.add_argument('-F', '--format-output', dest='o_format', default='{timestamp:15} [{source:10}] {tags:15}: {content}', metavar='FORMAT_STRING', help='Event output format string. Available properties are: id, tags, source, timestamp and content.')
+  parser.add_argument('-T', '--format-timestamp', dest='o_ts_format', default='%Y-%m-%d %H:%M:%S.%f%Z', metavar='DATE_FORMAT_STRING', help='Timestamp strftime compatible format string')
+
+
+def format_event(ev, fmt, datefmt=None):
+  dt = {
+    "id": ev.id,
+    "tags": ",".join(ev.tags),
+    "source": ev.source,
+    "content": ev.content
+  }
+  if datefmt:
+    try:
+        evt = datetime.fromtimestamp(ev.timestamp)
+        dt['timestamp'] = evt.strftime(datefmt)
+    except ValueError:
+        dt['timestamp'] = ev.timestamp
+  else:
+    dt["timestamp"] = ev.timestamp
+  
+  return fmt.format(**dt)
 
 def run_query(args):
   loop = asyncio.get_event_loop()
@@ -37,7 +61,7 @@ def run_query(args):
   
   result = None
   parser = EventParser('UTF-8')
-  fmt = '{timestamp} [{source}] {tags}: {content}'
+  
   
   def printev(evdata):
     if isinstance(evdata, str):
@@ -45,7 +69,7 @@ def run_query(args):
       return
     try:
       ev = parser.parse_event(BytesIO(evdata))
-      print(fmt.format(**ev.__dict__))
+      print(format_event(ev, args.o_format, args.o_ts_format))
     except Exception as e:
       print('> Failed to show event', e)
       try:
@@ -57,5 +81,9 @@ def run_query(args):
     result = query.live(cf, printev)
   else:
     result = query.find(cf, printev)
-  print('loop forever')
+  
+  loop.add_signal_handler(signal.SIGHUP, loop.stop)
+  loop.add_signal_handler(signal.SIGINT, loop.stop)
+  loop.add_signal_handler(signal.SIGTERM, loop.stop)
   loop.run_forever()
+  loop.close()
