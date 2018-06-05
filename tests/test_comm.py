39e1dc75839b2404a2f8aebb92568a3b640d1221
Pavle Jonoski <jonoski.pavle@gmail.com>
2018-06-06 01:59:09 +0200

    theia.comm module Server unit tests

--- tests/test_comm.py
+++ tests/test_comm.py
@@ -1,5 +1,5 @@
-from theia.comm import Client
-from theia.model import Event
+from theia.comm import Client, Server
+from theia.model import Event, EventSerializer
 import asyncio
 from unittest import mock
 from websockets import WebSocketClientProtocol as WebSocket
@@ -165,3 +165,180 @@ def test_client_send_event(m_send, m_recv, m_connect):
         print('Task:', t, 'cancelled.')
    
     evloop.close()
+
+
+def test_start_server():
+    loop = asyncio.new_event_loop()
+
+    ser = Server(loop=loop, host='localhost', port=11223)
+    
+    ser.start()
+    
+    assert ser.host == 'localhost'
+    assert ser.port == 11223
+    assert ser._started == True
+    
+    for t in asyncio.Task.all_tasks(loop):
+        t.cancel()
+    loop.stop()
+    loop.close()
+
+
+def test_start_and_stop_server():
+    loop = asyncio.new_event_loop()
+
+    ser = Server(loop=loop, host='localhost', port=11223)
+    
+    ser.start()
+    
+    assert ser.host == 'localhost'
+    assert ser.port == 11223
+    assert ser._started == True
+    
+    ser.stop()
+    
+    assert ser._started == True
+    
+    for t in asyncio.Task.all_tasks(loop):
+        t.cancel()
+    loop.stop()
+    loop.close()
+
+@mock.patch.object(WebSocket, 'recv')
+@mock.patch.object(WebSocket, 'send')
+def test_server_on_action(m_send, m_recv):
+    loop = asyncio.new_event_loop()
+
+    ser = Server(loop=loop, host='localhost', port=11223)
+    
+    serialized_event = EventSerializer().serialize(Event(id='00001',
+                                                         timestamp=10,
+                                                         source='src-1',
+                                                         tags=['t-1', 't-2'],
+                                                         content='content-1'))
+    state = {}
+    ws = WebSocket()
+    
+    async def fake_recv():
+        if state.get('event_yield'):
+            print('loop stop')
+            loop.stop()
+            await asyncio.sleep(10)
+        else:
+            state['event_yield'] = True
+        return serialized_event
+
+    m_recv.side_effect = fake_recv
+    
+    async def fake_send(msg):
+        pass
+    
+    m_send.side_effect = fake_send
+    
+    def action_handler(path, message, websocket, resp):
+        assert path == '/test-path'
+        assert message == serialized_event
+        assert websocket == ws
+        assert resp is ''
+        return 'RESP'
+   
+    on_action_mock = mock.MagicMock()
+    on_action_mock.side_effect = action_handler
+    
+    
+    
+    ser.on_action('/test-path', on_action_mock)
+    
+    ser.start()
+    
+    assert ser.host == 'localhost'
+    assert ser.port == 11223
+    assert ser._started == True
+    
+    asyncio.ensure_future(ser._on_client_connection( ws, '/test-path'), loop=loop)
+    loop.run_forever()
+    for t in asyncio.Task.all_tasks(loop):
+        t.cancel() # cancel the asyncio.wait
+        print('[CANCEL]',t)
+    loop.close()
+    
+    assert m_recv.call_count == 2 # once to get the message; second time to end the loop
+    assert on_action_mock.call_count == 1
+    assert m_send.call_count == 1
+    m_send.assert_called_once_with('RESP')
+
+
+@mock.patch.object(WebSocket, 'recv')
+@mock.patch.object(WebSocket, 'send')
+@mock.patch.object(WebSocket, 'close')
+def test_server_close_handler(m_close, m_send, m_recv):
+    from threading import Thread
+    from time import sleep
+    
+    loop = asyncio.new_event_loop()
+
+    ser = Server(loop=loop, host='localhost', port=11223)
+    ser._stop_timeout = 0.1
+    
+    serialized_event = EventSerializer().serialize(Event(id='00001',
+                                                         timestamp=10,
+                                                         source='src-1',
+                                                         tags=['t-1', 't-2'],
+                                                         content='content-1'))
+    state = {}
+    websocket = WebSocket()
+    
+    def ws_close_handler(ws, path):
+        assert ws == webocket
+        assert path == '/test-path'
+    
+    mock_handler = mock.MagicMock()
+    mock_handler.side_effect = ws_close_handler
+    
+    
+    async def fake_recv():
+        # at this point, the client websocket is registered.
+        success = ser.on_websocket_close(websocket, mock_handler)
+        assert success
+        
+        loop.call_soon(ser.stop)
+        await asyncio.sleep(0.4)
+        loop.call_soon(loop.stop)
+        await asyncio.sleep(10)
+
+    m_recv.side_effect = fake_recv
+    
+    async def fake_send(msg):
+        pass
+    
+    m_send.side_effect = fake_send
+    
+    ser.start()
+    
+    assert ser.host == 'localhost'
+    assert ser.port == 11223
+    assert ser._started == True
+    
+    def stop_server():
+        ser.stop()
+        print(' >> server stop')
+        sleep(1)
+        print(' >> about to loop.stop')
+        loop.call_soon_threadsafe(loop.stop)
+        print(' >> thread exit')
+    
+    t = Thread(target=stop_server)
+    
+    asyncio.ensure_future(ser._on_client_connection(websocket, '/test-path'), loop=loop)
+    #t.start()
+    loop.run_forever()
+    print(' *** loop done ***')
+    #t.join()
+    for t in asyncio.Task.all_tasks(loop):
+        t.cancel() # cancel the asyncio.wait
+        print('[CANCEL]',t)
+    loop.close()
+    
+    assert m_recv.call_count == 1
+    assert mock_handler.call_count == 1
+    assert m_close.call_count == 1
