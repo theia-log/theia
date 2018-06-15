97e4599d829a98735bc0808d9923087df21c0572
Pavle Jonoski <jonoski.pavle@gmail.com>
2018-06-15 20:40:42 +0200

    Improved code-style

--- theia/cli/collector.py
+++ theia/cli/collector.py
@@ -1,37 +1,65 @@
-from theia.cli.parser import get_parent_parser as parent_parser
+"""
+-------------------
+theia.cli.collector
+-------------------
+
+Theia collector command line interface script.
+"""
+import signal
+from logging import getLogger
 from theia.naivestore import NaiveEventStore
 from theia.collector import Collector
-import signal
+
+
+log = getLogger(__name__)
 
 
 def get_parser(subparsers):
+    """Configures the subparser for the ``collect`` command.
+
+    :param argparse.ArgumentParser subparser: subparser for commands.
+
+    Returns :class:`argparse.ArgumentParser` confured for the ``collect`` command.
+    """
     parser = subparsers.add_parser('collect', help='Collector server')
 
     parser.add_argument('-d', '--data-dir', dest='data_dir', help='Data store root directory')
-    parser.add_argument('-U', '--db-url', dest='db_url', help='Database URL (SQLAlchemy form)', default=None)
-    parser.add_argument('--verbose', dest='store_verbose', action='store_true', help='Make the EventStore to log more verbose output.')
-    parser.add_argument('--rdbs-store', dest='rdbs_store', action='store_true', help='Use RDBS EventStore instead of NaiveEventStore. The RDBS store keeps the events in a relational database.')
+    parser.add_argument('-U', '--db-url', dest='db_url', help='Database URL (SQLAlchemy form)',
+                        default=None)
+    parser.add_argument('--verbose', dest='store_verbose', action='store_true',
+                        help='Make the EventStore to log more verbose output.')
+    parser.add_argument('--rdbs-store', dest='rdbs_store', action='store_true',
+                        help='Use RDBS EventStore instead of NaiveEventStore.' +
+                        'The RDBS store keeps the events in a relational database.')
 
     return parser
 
 
 def run_collector(args):
+    """Runs the collector server.
+
+    :param argparse.Namespace args: arguments to configure the
+        :class:`theia.collector.Collector` instance.
+
+    """
     store = None
-    
+
     if args.rdbs_store:
         store = get_rdbs_store(args)
         if store:
-            print('Using RDBS Event Store')
+            log.info('Using RDBS Event Store')
         else:
-            print('Unable to set up the RDBS Event Store. Will fall back to using Naive Event Store.')
+            log.warning('Unable to set up the RDBS Event Store. Will fall back to using Naive Event Store.')
     if not store:
         store = NaiveEventStore(root_dir=args.data_dir)
-        print('Using Naive Event Store')
+        log.info('Using Naive Event Store')
 
     collector = Collector(store=store, hostname=args.server_host, port=args.port)
 
     def stop_collector(sig, frame):
-        print('Collector is shutting down.')
+        """Signal handler that stops the collector.
+        """
+        log.info('Collector is shutting down.')
         collector.stop()
 
     signal.signal(signal.SIGHUP, stop_collector)
@@ -48,14 +76,21 @@ def _has_sqlalchemy():
         return False
     return True
 
+
 def get_rdbs_store(args):
+    """Creates and configures new :class:`theia.rdbs.RDBSEventStore` based on
+    the arguments passed.
+
+    :param argparse.Namespace args: arguments.
+
+    """
     if not _has_sqlalchemy():
-        print('SQLAlchemy is not present on your system. RDBSEventStore cannot work without it.')
+        log.info('SQLAlchemy is not present on your system. RDBSEventStore cannot work without it.')
         return None
+
     from theia.rdbs import create_store
-    
+
     if not args.db_url:
         raise Exception('No Database URL')
-    
+
     return create_store(db_url=args.db_url, verbose=args.store_verbose)
-    
