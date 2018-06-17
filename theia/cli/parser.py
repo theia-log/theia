da298e30031d43d8d8cfc7c9ff6f26c8e5a0209e
Pavle Jonoski <jonoski.pavle@gmail.com>
2018-06-17 11:24:28 +0200

    Documentation for the parent CLI parser.

--- theia/cli/parser.py
+++ theia/cli/parser.py
@@ -1,7 +1,25 @@
+"""
+----------------
+theia.cli.parser
+----------------
+
+
+Theia CLI main :mod:`argparse` parser.
+"""
 import argparse
 
 
 def get_parent_parser(name, desc=''):
+    """Creates the main (parent) :class:`argparse.ArgumentParser` for Theia CLI.
+
+    Defines the main argument options such as theia server host, port, verbosity
+    level etc.
+
+    :param str name: the name of the program.
+    :param str desc: program description.
+
+    Returns the configured :class:`argparse.ArgumentParser`.
+    """
     parser = argparse.ArgumentParser(prog=name, description=desc)
 
     parser.add_argument('-v', '--version',
@@ -9,7 +27,7 @@ def get_parent_parser(name, desc=''):
     parser.add_argument('-H', '--host', help='Hostname to bind to',
                         default='localhost', dest='server_host')
     parser.add_argument('-P', '--port', help='Listen on port', default=6433)
-    
+
     parser.add_argument('--verbose', dest='verbose', action='store_true',
                         help='Verbose output.')
 
