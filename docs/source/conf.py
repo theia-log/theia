51f4db71e29fd6a834c4d345a5e133aa5a64a250
Pavle Jonoski <jonoski.pavle@gmail.com>
2018-06-19 01:41:05 +0200

    Theia version 0.9.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ author = 'Pavle Jonoski'
 # The short X.Y version
 version = '0.9'
 # The full version, including alpha/beta/rc tags
-release = '0.9.1'
+release = '0.9.2'
 
 
 # -- General configuration ---------------------------------------------------
