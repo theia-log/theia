0b2116d1027d9ab89e70d0b2170bb9056f3065bd
Pavle Jonoski <jonoski.pavle@gmail.com>
2018-06-19 01:28:20 +0200

    theia.cli.query run_query unit tests.

--- tests/test_cli_query.py
+++ tests/test_cli_query.py
@@ -1,4 +1,6 @@
 from theia.cli.query import get_parser, format_event, event_printer, run_query
+from theia.query import Query, ResultHandler
+import asyncio
 from argparse import ArgumentParser
 from io import StringIO
 from unittest import mock
@@ -110,3 +112,138 @@ def test_event_printer(m_stdout):
     
     assert m_stdout.getvalue() == 'id-0|2018-06-17|/source/1|a,b|event 1\n'
 
+
+@mock.patch.object(Query, 'live')
+@mock.patch.object(ResultHandler, 'when_closed')
+@mock.patch.object(asyncio, 'get_event_loop')
+def test_run_query_live(m_get_event_loop, m_when_closed, m_live):
+    from collections import namedtuple
+    from threading import Thread
+    from time import sleep
+    
+    Namespace = namedtuple('Namespace', ['server_host', 'port', 'live', 'f_source',
+                                        'f_id', 'f_after', 'f_before', 'f_content',
+                                        'f_tags','f_order', 'o_format', 'o_ts_format',
+                                        'close_timeout'])
+    
+    args = Namespace(server_host='localhost',
+                     port=1123,
+                     live=True,
+                     f_source='src-pattern',
+                     f_id='id-pattern',
+                     f_after=1000,
+                     f_before=2000,
+                     f_content='content-pattern',
+                     f_tags=['a', 'b'],
+                     f_order='order-pattern',
+                     o_format='out-format',
+                     o_ts_format='ts-format',
+                     close_timeout=20)
+   
+    loop = asyncio.new_event_loop()
+    m_get_event_loop.return_value = loop
+   
+    state = {}
+   
+    def fake_when_closed(handler):
+        state['when_closed'] = handler
+    
+    m_when_closed.side_effect = fake_when_closed
+    
+    result_handler = ResultHandler(client=mock.MagicMock())
+    
+    def fake_live(crt, cb):
+        assert cb is not None
+        assert crt.get('source') == 'src-pattern'
+        assert crt.get('id') == 'id-pattern'
+        assert crt.get('start') == 1000
+        assert crt.get('end') == 2000
+        assert crt.get('content') == 'content-pattern'
+        assert crt.get('tags') == ['a', 'b']
+        
+        return result_handler
+   
+    m_live.side_effect = fake_live
+    
+    
+    def stop_loop():
+        sleep(0.4)
+        assert state.get('when_closed') is not None
+        loop.call_soon_threadsafe(state['when_closed'])
+    
+    t = Thread(target=stop_loop)
+    t.start()
+    run_query(args)
+    t.join()
+    assert m_live.call_count == 1
+    assert m_get_event_loop.call_count == 1
+    assert m_when_closed.call_count == 1
+         
+
+@mock.patch.object(Query, 'find')
+@mock.patch.object(ResultHandler, 'when_closed')
+@mock.patch.object(asyncio, 'get_event_loop')
+def test_run_query_find(m_get_event_loop, m_when_closed, m_find):
+    from collections import namedtuple
+    from threading import Thread
+    from time import sleep
+    
+    Namespace = namedtuple('Namespace', ['server_host', 'port', 'live', 'f_source',
+                                        'f_id', 'f_after', 'f_before', 'f_content',
+                                        'f_tags','f_order', 'o_format', 'o_ts_format',
+                                        'close_timeout'])
+    
+    args = Namespace(server_host='localhost',
+                     port=1123,
+                     live=False,
+                     f_source='src-pattern',
+                     f_id='id-pattern',
+                     f_after=1000,
+                     f_before=2000,
+                     f_content='content-pattern',
+                     f_tags=['a', 'b'],
+                     f_order='order-pattern',
+                     o_format='out-format',
+                     o_ts_format='ts-format',
+                     close_timeout=20)
+   
+    loop = asyncio.new_event_loop()
+    m_get_event_loop.return_value = loop
+   
+    state = {}
+   
+    def fake_when_closed(handler):
+        state['when_closed'] = handler
+    
+    m_when_closed.side_effect = fake_when_closed
+    
+    result_handler = ResultHandler(client=mock.MagicMock())
+    
+    def fake_find(crt, cb):
+        assert cb is not None
+        assert crt.get('source') == 'src-pattern'
+        assert crt.get('id') == 'id-pattern'
+        assert crt.get('start') == 1000
+        assert crt.get('end') == 2000
+        assert crt.get('content') == 'content-pattern'
+        assert crt.get('tags') == ['a', 'b']
+        
+        return result_handler
+   
+    m_find.side_effect = fake_find
+    
+    
+    def stop_loop():
+        sleep(0.4)
+        assert state.get('when_closed') is not None
+        loop.call_soon_threadsafe(state['when_closed'])
+    
+    t = Thread(target=stop_loop)
+    t.start()
+    run_query(args)
+    t.join()
+    assert m_find.call_count == 1
+    assert m_get_event_loop.call_count == 1
+    assert m_when_closed.call_count == 1
+         
+    
